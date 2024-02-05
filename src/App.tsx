79e7455826b52a199dad1232e763f83319ac8f77
semih <semihcaliskan0103@gmail.com>
2024-02-05 15:46:30 +0300

    try

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,28 @@ import {
     Image, Center, Text,
     Avatar, AvatarBadge, AvatarGroup, Menu, MenuButton, MenuList, MenuItem
 } from "@chakra-ui/react";
-import {ColorModeToggler} from "./theme/color-mode-toggler";
-import {Link, useNavigate} from "react-router-dom";
+import { ColorModeToggler } from "./theme/color-mode-toggler";
+import { Link, useNavigate } from "react-router-dom";
 
 function App() {
 
 
     return (
         <div>
-            <Flex w='100%' p={4} mb={100} mt={30}>
+            <Flex
+                as="nav"
+                align="center"
+                justify="space-between"
+                wrap="wrap"
+                padding="1.5rem"
+                color="white"
+                position="fixed"
+                top="0"
+                width="100%"
+                zIndex="1000" w='100%' p={4} >
                 <Center w='100px'>
                     <Image src={require('./assets/drive.png')} boxSize='100px'
-                           objectFit='cover' borderRadius='full' alt='Dan Abramov'/>
+                        objectFit='cover' borderRadius='full' alt='Dan Abramov' />
                 </Center>
 
                 <Center flex={1}>
@@ -32,17 +42,15 @@ function App() {
                     <Text>home</Text>
                 </Center>
 
-                <Spacer/>
+                <Spacer />
                 <Center>
-
                     <Menu>
-
                         <MenuButton>
-                            <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov'/>
+                            <Avatar name='Dan Abrahmov' src='https://bit.ly/dan-abramov' />
                         </MenuButton>
                         <MenuList>
                             <MenuItem>
-                                <Link to="/your-route" >
+                                <Link to="/your-route">
                                     Your Menu Item
                                 </Link>
                             </MenuItem>
@@ -55,8 +63,13 @@ function App() {
                 </Center>
 
             </Flex>
-            <ColorModeToggler/>
-            <Button colorScheme='purple'>hello chakra</Button>
+            <Box mt={200}>
+                <ColorModeToggler />
+                <Button colorScheme='purple'>hello chakra</Button>
+
+            </Box>
+
+
 
         </div>
     );
